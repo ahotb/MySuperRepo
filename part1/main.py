67d4bdaa41e3322d83a59e5c3e552451143c7524
ahotb <ah.r9625@gmail.com>
2026-04-22 14:43:55 +0300

    Add create_place function with validation and database insertion logic

--- part1/main.py
+++ part1/main.py
@@ -2,28 +2,42 @@ from data.initial_data import init_schema
 from services.user_service import register_user, login_user
 from core.database import fetch_all
 from services.amenity_service import create_amenity
+from services.place_service import create_place
+
 if __name__ == "__main__":
     init_schema()
 
-    print(create_amenity(
-        {"name": "swiim", "description": "AC in outsied", "images": None}))
-    print(create_amenity(
-        {"name": "", "description": "dec", "images": None}))  # خطأ
-    # valid_user = {
-    #     "username": "ailnl",
-    #     "email": "aisl@gml.co",
-    #     "password": "123456910"
-    # }
-    # print("🔹 Test 1: Valid Login (Success):")
-    # result = login_user("aisl@gml.co", "123456910")
-    # print(result)
-    # print("\n" + "#" * 50 + "\n")
 
-    # invalid_user = {
-    #     "username": "ailnl",
-    #     "email": "aisl@gml.co",
-    #     "password": "123456910"
-    # }
-    # print("🔹 Test 2: Wrong Password (Fail):")
-    # result_invalid = login_user("ailnl", "124545156910")
-    # print(result_invalid)
+# تأكد أن user_id و amenity_id موجودين فعلاً في قاعدة بياناتك قبل التجربة
+print(create_place({
+    "title": "fali",
+    "description": "Wonderful and spacious apartments",
+    "price": 1500.0,
+    "latitude": 21.4225,
+    "longitude": 39.8262,
+    "amenity_id": 5,  # استبدله بمعرف وسيلة راحة موجودة
+    "user_id": 1      # استبدله بمعرف مالك موجود
+}))
+
+# print(create_amenity(
+#     {"name": "swiim", "description": "AC in outsied", "images": None}))
+# print(create_amenity(
+#     {"name": "", "description": "dec", "images": None}))  # خطأ
+# valid_user = {
+#     "username": "ailnl",
+#     "email": "aisl@gml.co",
+#     "password": "123456910"
+# }
+# print("🔹 Test 1: Valid Login (Success):")
+# result = login_user("aisl@gml.co", "123456910")
+# print(result)
+# print("\n" + "#" * 50 + "\n")
+
+# invalid_user = {
+#     "username": "ailnl",
+#     "email": "aisl@gml.co",
+#     "password": "123456910"
+# }
+# print("🔹 Test 2: Wrong Password (Fail):")
+# result_invalid = login_user("ailnl", "124545156910")
+# print(result_invalid)
---
+++ part1/services/place_service.py
@@ -0,0 +1,35 @@
+from models.place import Place
+from core.database import fetch_one, execute_query
+
+
+def create_place(place_data):
+    place_obj = Place(**place_data)
+
+    if not place_obj.is_valid():  # التحقق من وجود البيانات
+        return {"is_valid": False, "data": None, "errors": place_obj.get_errors()}
+    # استعاء البيانات كالقموس
+    place_dict = place_obj.to_dict()
+    # البحث عن وجود المالك
+    existing_user = fetch_one(
+        "SELECT id FROM users WHERE id=?",
+        (place_dict["user_id"],))
+    # اذا لم يكون موجود يكون خطأ
+    if existing_user is None:
+        return {"is_valid": False, "errors": ["Owner not found."]}
+    # البحث عن وسائل الراحه
+    existing_amenity = fetch_one(
+        "SELECT id FROM amenity WHERE id=?",
+        (place_dict["amenity_id"],))
+    # اذا لم تكن موجوده خطأ
+    if existing_amenity is None:
+        return {"is_valid": False, "errors": ["Amenity not found."]}
+    # هنا اعداد اضافة المكان بعد الانتهاء من التحقق
+    insert_sql = """INSERT INTO place ( title, description, price, latitude,longitude, amenity_id, user_id)
+    VALUES (?,?,?,?,?,?,?)"""
+    values_to_insert = (place_dict["title"], place_dict["description"], place_dict["price"], place_dict["latitude"],
+                        place_dict["longitude"], place_dict["amenity_id"], place_dict["user_id"])
+    is_saved = execute_query(insert_sql, values_to_insert)
+    # هنا تأكيد الاضافه او العكس
+    if is_saved:
+        return {"is_valid": True, "message": "place registered successfully."}
+    return {"is_valid": False, "errors": ["Failed to save data. Please try again."]}
