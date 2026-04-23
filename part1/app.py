8ee46c76a3e00237753ee1a87e0a2c4f908983b2
ahotb <ah.r9625@gmail.com>
2026-04-23 22:00:56 +0300

    Add API endpoint for fetching places with error handling

--- part1/app.py
+++ part1/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from services.user_service import register_user, login_user
+from core.database import fetch_all
 # استدعاء المكتبه لتشغيل السيرفر
 app = Flask(__name__)
 
@@ -7,7 +8,7 @@ app = Flask(__name__)
 
 
 @app.route('/api/register', methods=['POST'])
-def api_register():
+def api_register():  # مسار انشاء حساب جديد
     user_data = request.get_json()  # هنا ناخذ البيانات ونحولها ل قاموس
     role = user_data.get('role', 'user')  # هنا ندع المستخدم يكون افتراضي
     try:  # استخدمة تري لكي لا ينهار البرنامج
@@ -22,7 +23,7 @@ def api_register():
 
 
 @app.route('/api/login', methods=['POST'])
-def api_login():
+def api_login():  # مسار تسجيل الدخول
     user_data = request.get_json()
     # هنا حددنا من القاموس الي نحتاجه وهو الايميل والمغير الثاني نفسه
     user_email = user_data.get('email')
@@ -39,6 +40,23 @@ def api_login():
     except Exception as e:
         print(f"Error {e}")
         return jsonify({"error": "Database save failed"}), 500
+
+
+@app.route('/api/places', methods=["GET"])
+def get_places():  # مسار عرض الاماكن
+    try:
+        places = fetch_all(
+            "SELECT id, title, description, price, latitude, longitude, amenity_id, user_id FROM place")
+
+        place_data = []
+        for row in places:
+            place_data.append({"id": row[0], "title": row[1], "description": row[2], "price": row[3],
+                               "latitude": row[4], "longitude": row[5], "amenity_id": row[6], "user_id": row[7]})
+        return jsonify({"places": place_data}), 200
+    except Exception as e:
+        print(f"Error {e}")
+        return jsonify({"error": "Database save failed"}), 500
+
 
     # امر تشغيل السيرفر
 if __name__ == "__main__":
